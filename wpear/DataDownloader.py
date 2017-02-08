1fde9aa75830fa8d493bcfab89803f1bd3a8f488
Lala De <lvde21@live.com>
2017-02-08 15:30:35 -0500

    added progress indicator for downloads

--- wpear/DataDownloader.py
+++ wpear/DataDownloader.py
@@ -4,7 +4,7 @@ import sys
 
 from ftplib import FTP
 
-		
+
 
 class DataDownloader():
 	'Class to download forecast/observation files'
@@ -17,16 +17,16 @@ class DataDownloader():
 	def __init__(self, server, directory):
 		if server is not None:
 			self.ftpServerForForecasts = server
-		
+
 		if directory is not None:
 			self.defaultFTPDirectoryForForecasts = directory
-		
+
 		try:
 			self.ftp = FTP(self.ftpServerForForecasts)
 			self.ftp.login()
 		except:
 			sys.exit('Error connecting to FTP server')
-			
+
 		try:
 			self.ftp.cwd(self.defaultFTPDirectoryForForecasts)
 		except:
@@ -36,13 +36,36 @@ class DataDownloader():
 
 	# Issue: Which file to download?	
 	def retrieveLatestFile(self):
+
 		self.ftp.cwd(self.ftp.nlst('-t')[0]) # dates
 		filename = self.ftp.nlst('-t')[0] # no idea what these files represent
-		
-		print('downloading file : ' + filename)
+
+		print('Downloading file : ' + filename)
+		self.filesize = float(self.ftp.size(filename)) 
+		self.fileDownloadPercent = 0.0
+		self.lastShownPercent = 0.0
 
 		with open('%s' % filename, mode='wb') as target:
-			self.ftp.retrbinary('RETR %s' % filename, target.write)
+			self.currsize = 0
+
+			def callback(chunk):
+				
+				if self.currsize == 0:
+					print 'Downloading...0.0%...',
+				
+				chunksize = float(len(chunk))
+				target.write(chunk)
+				
+				self.currsize += float(chunksize)
+				self.fileDownloadPercent = round(float(self.currsize/self.filesize) * 100)
+				
+				if self.fileDownloadPercent % 5 == 0 and self.fileDownloadPercent > self.lastShownPercent:
+					print str(self.fileDownloadPercent) + '%...',
+					# debug print sizes
+					# print('[' + str(self.currsize) + '/' + str(self.filesize) + ']')
+					self.lastShownPercent = self.fileDownloadPercent
+
+			self.ftp.retrbinary('RETR %s' % filename, callback, 32768)
 
 
 
@@ -51,6 +74,6 @@ class DataDownloader():
 			self.ftp.quit()	
 
 
-
-d = DataDownloader(None, None)
-d.retrieveLatestFile()	
+# debug run
+# d = DataDownloader(None, None)
+# d.retrieveLatestFile()	
