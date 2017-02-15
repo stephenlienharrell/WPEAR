a7e6fdd0e788b10e737576a6a1546e055df83e12
Lala De <lvde21@live.com>
2017-02-15 15:31:56 -0500

    implemented download, listDirectory for HTTP file server links

--- wpear/DataDownloader.py
+++ wpear/DataDownloader.py
@@ -1,79 +1,47 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
-import sys
+import urllib, os, urlparse, requests
+from bs4 import BeautifulSoup
 
-from ftplib import FTP
-
-
+# only works for HTTP links (no FTP links)
 
 class DataDownloader():
 	'Class to download forecast/observation files'
 
-	ftpServerForForecasts = 'ftp.ncep.noaa.gov'
-	defaultFTPDirectoryForForecasts = '/pub/data/nccf/com/hrrr/prod'
+	ext = ('grib2.gz', 'grib2')
+
+	def download(self, url, filedir, defaultdownloaddir='/sourceFileDownloads'):
+		downloadDirectory = os.getcwd() + defaultdownloaddir
+		if not os.path.exists(downloadDirectory):
+			os.makedirs(downloadDirectory)
+
+		fullURL = urlparse.urljoin(url, filedir)
+		file = urllib.URLopener()
+
+		fileDownloadPath = downloadDirectory + '/' + fullURL.split('/')[-1]
+		file.retrieve(fullURL, fileDownloadPath)
+		return fileDownloadPath
+
+
+	def listDirectory(self, url):
+		page = requests.get(url).text
+		soup = BeautifulSoup(page, 'html.parser')
+		return [url  + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(self.ext)]
+
+	
+	def changeExtentions(self, newExt):
+		self.ext = newExt
+		print self.ext	
 
 
 
-	def __init__(self, server, directory):
-		if server is not None:
-			self.ftpServerForForecasts = server
 
-		if directory is not None:
-			self.defaultFTPDirectoryForForecasts = directory
+# test inputs
+# dd = DataDownloader()
+# ==== test download ====
+# print dd.download('http://mrms.ncep.noaa.gov/data', 'data/3DReflPlus/MergedReflectivityQC_00.50/MRMS_MergedReflectivityQC_00.50_20170214-180435.grib2.gz')
+# print dd.download('http://www.ftp.ncep.noaa.gov/data', 'data/nccf/com/hrrr/prod/hrrr.20170215/hrrr.t05z.wrfsfcf16.grib2.idx')
+# ==== test listDirectory =====
+# print dd.listDirectory('http://www.ftp.ncep.noaa.gov/data/nccf/com/hrrr/prod/hrrr.20170215/')
+# print dd.listDirectory('http://mrms.ncep.noaa.gov/data/3DReflPlus/MergedReflectivityQC_00.50/')
 
-		try:
-			self.ftp = FTP(self.ftpServerForForecasts)
-			self.ftp.login()
-		except:
-			sys.exit('Error connecting to FTP server')
-
-		try:
-			self.ftp.cwd(self.defaultFTPDirectoryForForecasts)
-		except:
-			sys.exit('Error changing directory')
-
-
-
-	# Issue: Which file to download?	
-	def retrieveLatestFile(self):
-
-		self.ftp.cwd(self.ftp.nlst('-t')[0]) # dates
-		filename = self.ftp.nlst('-t')[0] # no idea what these files represent
-
-		print('Downloading file : ' + filename)
-		self.filesize = float(self.ftp.size(filename)) 
-		self.fileDownloadPercent = 0.0
-		self.lastShownPercent = 0.0
-
-		with open('%s' % filename, mode='wb') as target:
-			self.currsize = 0
-
-			def callback(chunk):
-				
-				if self.currsize == 0:
-					print 'Downloading...0.0%...',
-				
-				chunksize = float(len(chunk))
-				target.write(chunk)
-				
-				self.currsize += float(chunksize)
-				self.fileDownloadPercent = round(float(self.currsize/self.filesize) * 100)
-				
-				if self.fileDownloadPercent % 5 == 0 and self.fileDownloadPercent > self.lastShownPercent:
-					print str(self.fileDownloadPercent) + '%...',
-					# debug print sizes
-					# print('[' + str(self.currsize) + '/' + str(self.filesize) + ']')
-					self.lastShownPercent = self.fileDownloadPercent
-
-			self.ftp.retrbinary('RETR %s' % filename, callback, 32768)
-
-
-
-	def __del__(self):
-		if hasattr(self, 'ftp'):
-			self.ftp.quit()	
-
-
-# debug run
-# d = DataDownloader(None, None)
-# d.retrieveLatestFile()	
