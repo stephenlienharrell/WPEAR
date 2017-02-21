ff95359314aabd26a90b3fb4a16f98381cf6a068
ddoshi26 <doshid@purdue.edu>
2017-02-21 14:46:22 -0800

    Completed returning the grib message

--- wpear/DataComparator.py
+++ wpear/DataComparator.py
@@ -9,8 +9,15 @@ def DataComparator(forecast_file, observed_file):
 	temp15_arr_f = grbs_f.select(name='2 metre temperature')[0]
 	temp15_arr_o = grbs_o.select(name='2 metre temperature')[0]
 	
-	# Only doing difference for now
+	#grb = grbs_f.message(12)
+	#print grb
+	#Only doing difference for now
 	arr = temp15_arr_f.values - temp15_arr_o.values
 	#print arr
+	msg = arr.tostring()
+	print msg
 
 	# return grib message
+	return msg
+
+DataComparator('hrrr.t15z.wrfsubhf01.grib2', 'hrrr.t15z.wrfsubhf01.grib2')
