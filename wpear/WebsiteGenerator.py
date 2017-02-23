f21d6e7133d5e6f1309289c41fb46a7f5903f222
Stephen Lien Harrell <sharrell@purdue.edu>
2017-02-23 17:43:47 -0500

    update website generator per instructions

--- wpear/WebsiteGenerator.py
+++ wpear/WebsiteGenerator.py
@@ -5,15 +5,15 @@ def showWebsite(forecast_image, observed_image, compared_image):
     html_file = open('main.html', 'w+')
     file_head = """<html><head>
     <title>WPEAR Website</title>
-    <body><p><center>WPEAR Website</center></p>
+    <body><center><p>WPEAR Website</p>
     <p>Displaying visualizations</p>
     <h2>Forecast 1 Visualization</h2>
-    <img src='""" + forecast_image + """' alt="Forecast file" style="width:304px;height:228px;">
+    <img src='""" + forecast_image + """' alt="Forecast file" >
     <h2>Forecast 2 Visualization</h2>
-    <img src='""" + observed_image + """' alt="Observed file" style="width:304px;height:228px;">
+    <img src='""" + observed_image + """' alt="Observed file" >
     <h2>Compared Visualization</h2>
-    <img src='""" + compared_image + """' alt="Compared file" style="width:304px;height:228px;">
-    </body></html>"""
+    <img src='""" + compared_image + """' alt="Compared file" >
+    </center></body></html>"""
 
     html_file.write(file_head)
     html_file.close()
