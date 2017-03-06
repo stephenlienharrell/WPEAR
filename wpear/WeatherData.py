2bd8cb3138e4a22f84676ad05da7ce1169a2395b
sharrell@purdue.edu <sharrell@purdue.edu>
2017-03-06 12:40:15 -0500

    Add threading for faster proccessing

--- wpear/WeatherData.py
+++ wpear/WeatherData.py
@@ -1,6 +1,8 @@
-import errno    
+import errno
+import multiprocessing
 import os
 import shutil
+import time
 
 import pygrib
 
@@ -8,6 +10,7 @@ import DataDownloader
 import DataConverter
 import DataVisualizer
 
+THREAD_MAX=20
 
 class WeatherData(object):
 
@@ -17,6 +20,9 @@ class WeatherData(object):
         self.date = date
         self.vars = vars
         self.domain = domain
+        
+        self.threads = []
+        self.thread_count = 0
 
     def MakeDirs(self, path):
         try:
@@ -42,7 +48,6 @@ class WeatherData(object):
         if not os.path.exists(self.temp_directory):
             os.makedirs(self.temp_directory)
 
-        downloader = DataDownloader.DataDownloader()
 
        
         for index, file_name in enumerate(self.files_to_download):
@@ -51,18 +56,12 @@ class WeatherData(object):
                 continue
 
             file_directory = self.url_directory + file_name
-            try:
-                downloader.download(self.url, file_directory, self.temp_directory)
-            except IOError, e:
-                try:
-                    if e[1] != 404:
-                        raise
-                except:
-                    raise
-                continue
 
-            print "Download completed for %s" % file_directory
+            self._addToThreadPool(_doDownload, (self.url, file_directory, self.temp_directory))
+            self._waitForThreadPool()
 
+
+        self._waitForThreadPool(thread_max=0)
 
     def ConvertData(self):
         needed_vars = ['files_to_download', 'local_directory', 'converted_files']
@@ -81,29 +80,28 @@ class WeatherData(object):
             converted_file = self.converted_files[index]
             if os.path.exists(converted_file):
                 continue
-            DataConverter.convert(temp_file, converted_file)
-            print "Conversion completed for " + temp_file
-            converted_files.append(converted_file)
-            
-        return converted_files
+            self._addToThreadPool(_doConversion, (temp_file, converted_file))
+            self._waitForThreadPool()
 
+        self._waitForThreadPool(thread_max=0)
+            
     def VisualizeData(self):
         needed_vars = ['vars', 'visualization_heatmap_files', 'converted_files']
         self.CheckVars('VisualizeData', needed_vars)
         print "Starting data visualization"
-        visualizer = DataVisualizer.DataVisualizer(None)
         for index, file_name in enumerate(self.converted_files):
             if not os.path.exists(file_name):
                 continue
 
-            if os.path.exists(self.visualization_heatmap_files[index]):
+            out_file = self.visualization_heatmap_files[index]
+            if os.path.exists(out_file):
                 continue
 
-            grib_loaded = pygrib.open(file_name)
-            #for var in self.vars:
-            grib_msg = grib_loaded.select(name='2 metre temperature')[0]
-            visualizer.Heatmap(grib_msg, self.visualization_heatmap_files[index])
-            print "Visualizing " + self.visualization_heatmap_files[index] + " is complete"
+            self._addToThreadPool(_doVisualization, (file_name, out_file))
+            self._waitForThreadPool()
+
+        self._waitForThreadPool(thread_max=0)
+
 
     def CleanupDownloads(self):
         #TODO:  need to cleanup date based parent directories
@@ -112,3 +110,50 @@ class WeatherData(object):
     def VisualizeDifference(self, forecast):
         if not self.obs:
             raise ValueError('Must call VisualDifference on observations only')
+        for converted_file in self.converted_files:
+            date = self._GetTimeOfObs(converted_file)
+
+    def _addToThreadPool(self, function, args):
+        proc = multiprocessing.Process(target=function, args=args)
+        proc.start()
+        self.threads.append(proc)
+        self.thread_count += 1
+
+    def _waitForThreadPool(self, thread_max=THREAD_MAX - 1):
+        count = 0
+        while len(self.threads) > thread_max:
+            time.sleep(.1)
+            if count > len(self.threads) - 1:
+                count = 0
+            if self.threads[count].exitcode is not None:
+                self.threads[count].join(1)
+                self.threads.pop(count)
+            else: 
+                count += 1
+
+
+def _doDownload(url, file_directory, temp_directory):
+    try:
+        downloader = DataDownloader.DataDownloader()
+        downloader.download(url, file_directory, temp_directory)
+    except IOError, e:
+        try:
+            if e[1] != 404:
+                raise
+        except:
+            raise
+        return
+    print "Download completed for %s" % file_directory
+
+
+def _doConversion(temp_file, converted_file):
+    DataConverter.convert(temp_file, converted_file)
+    print "Conversion completed for " + temp_file
+
+def _doVisualization(file_name, out_file):
+    visualizer = DataVisualizer.DataVisualizer(None)
+    grib_loaded = pygrib.open(file_name)
+    #for var in self.vars:
+    grib_msg = grib_loaded.select(name='2 metre temperature')[0]
+    visualizer.Heatmap(grib_msg, out_file)
+    print "Visualizing " + out_file + " is complete"
