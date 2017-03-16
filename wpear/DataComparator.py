89c42b0c9ecee3d24c39dfde6ab3633d6115966f
Dhairya Doshi <doshid@purdue.edu>
2017-03-16 18:31:25 -0400

    Changed parameters to take in messages instead of grib objects

--- wpear/DataComparator.py
+++ wpear/DataComparator.py
@@ -1,24 +1,20 @@
 import pygrib
 import numpy
 
-def DataComparator(f_obj, o_obj): 
+def DataComparator(f_msg, o_msg):
 	#Potentially add column name too once we have more than one column in the cleaned file
 	#Comment out file opens if the file is sent
 	#grbs_f = pygrib.open(forecast_file)
 	#grbs_o = pygrib.open(observed_file)
-	
-	temp15_arr_f = f_obj.select(name='2 metre temperature')[0]
-	temp15_arr_o = o_obj.select(name='2 metre temperature')[0]
 	
 	#grb = grbs_f.message(12)
 	#lats,lons = grb.latlons()
 	#print lats.min(), lats.max()
 	#print lons.min(), lons.max()
 	#Only doing difference for now
-	arr = temp15_arr_f.values - temp15_arr_o.values
+	arr = f_msg.values - o_msg.values
 	#print arr
-	ret_file = f_obj
-	gobj = ret_file.select(name='2 metre temperature')[0]
+	gobj = f_msg
 	gobj.values=numpy.array(arr)
 	
 	print gobj.values
@@ -26,19 +22,19 @@ def DataComparator(f_obj, o_obj):
 	# return grib message
 	return gobj
 
-def MultiSetAverage(grb_list, var):	# To change to index referencing: MultiSetAverage(grb_list, index)
+def MultiSetAverage(grb_list):	# To change to index referencing: MultiSetAverage(grb_list, index)
 	size = len(grb_list)
 
 	if size <= 0:
-		return null
+		return None
 	else:		
 		#print grb_list[0].select(name=var)[0].values
 
-		arr = grb_list[0].select(name=var)[0].values	# Index referencing: arr = grb_list[0].message(index).values
+		arr = grb_list[0].values	# Index referencing: arr = grb_list[0].message(index).values
 		del grb_list[0]
 		
 		for grb_obj in grb_list:
-			arr = arr + grb_obj.select(name=var)[0].values		# Index referencing: arr = arr + grb_obj.message(index).values
+			arr = arr + grb_obj.values		# Index referencing: arr = arr + grb_obj.message(index).values
 		
 		arr = arr/size
 		#print arr
