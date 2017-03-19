fd5a0b5055bfd1a2144fef2dd1fa010643396c73
maoxia <luo149@purdue.edu>
2017-03-19 19:31:38 -0400

    created gif with fixed but not pretty colorbar

--- wpear/DataVisualizer.py
+++ wpear/DataVisualizer.py
@@ -6,9 +6,10 @@ import pygrib
 import random
 import imageio
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -84,6 +85,7 @@ class DataVisualizer():
                         cmap=plt.cm.jet)
 
         cbar = plt.colorbar(cs,location='bottom', fraction=0.046, pad=0.06)
+        
         # Adjust the position of Unit
         cbar_ax = cbar.ax
         cbar_ax.text(0.0, -1.3, unit, horizontalalignment='left')
@@ -91,6 +93,55 @@ class DataVisualizer():
         plt.title(data_type)
         plt.savefig(file_name)
         plt.close(fig)
+
+    
+    def Frame(self, grib_object, file_name, vmin, vmax):
+        """Generate a Heatmap as one frame for final GIF product
+        grib_object:    an object containing raw data to be visualized
+        file_name:   a string representing the name of generated picture
+        vmin:        lower limit
+        vmax:        upper limit 
+        """
+        data = grib_object.values
+        lat,lon = grib_object.latlons()
+        unit = grib_object['units']
+        data_type = grib_object['name']
+
+        fig = plt.figure(figsize=(8,8))
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+        m = Basemap(
+                resolution='c', # c, l, i, h, f or None
+                projection='cyl',
+                lat_0=39.72, lon_0=-86.29,
+                llcrnrlon=-87.79, llcrnrlat= 38.22,
+                urcrnrlon=-84.79, urcrnrlat=41.22)
+
+        parallels = np.arange(38.22, 41.22, 0.5)
+        m.drawparallels(parallels,labels=[False,True,True,False])
+        meridians = np.arange(-87.79, -84.79, 0.5)
+        m.drawmeridians(meridians,labels=[True,False,False,True])
+
+        x,y = m(lon, lat)
+        bounds = np.linspace(vmin, vmax, 10)
+        norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
+        # norm = mpl.colors.Normalize(vmin=vmin,vmax=vmax)
+
+        cs = m.pcolormesh(x,y,data,
+                        shading='gouraud',
+                        norm=norm,
+                        cmap=plt.cm.jet)
+
+
+        cbar = plt.colorbar(cs,location='bottom', fraction=0.046, pad=0.06)
+        
+        # Adjust the position of Unit
+        cbar_ax = cbar.ax
+        cbar_ax.text(0.0, -1.3, unit, horizontalalignment='left')
+        m.readshapefile(self.shapeFile,'areas')
+        plt.title(data_type)
+        plt.savefig(file_name)
+        plt.close(fig)    
 
 
     def Animated(self, grib_objects, file_name):
@@ -101,11 +152,24 @@ class DataVisualizer():
         filenames = []
         images = []
         count = 0
+        vmin = sys.maxint
+        vmax = -vmin - 1
+
         while (count < len(grib_objects)):
             filenames.append('tmp/' + 'pic_' + str(count) + '.jpg')
-            self.Heatmap(grib_objects[count], filenames[count])
-            print 'Generated ' + filenames[count]
+            # Get min and max among all data
+            if grib_objects[count]['maximum'] > vmax:
+                vmax = grib_objects[count]['maximum']
+
+            if grib_objects[count]['minimum'] < vmin:
+                vmin = grib_objects[count]['minimum']
             count += 1
+
+        while (count > 0):
+            index = len(grib_objects) - count
+            self.Frame(grib_objects[index], filenames[index], vmin, vmax)
+            print 'File ' + filenames[index] + " generated"
+            count -= 1
 
         for filename in filenames:
             images.append(imageio.imread(filename))
@@ -114,8 +178,8 @@ class DataVisualizer():
         imageio.mimsave(file_name, images, 'GIF', **kargs)
 
         # remove tmp files in tmp dir
-        for f in filenames:
-            os.remove(f)
+        # for f in filenames:
+            # os.remove(f)
 
 
     def SimplePlot(self, grib_object, file_name):
@@ -236,7 +300,6 @@ for root, dirs, filenames in os.walk(indir):
         msgs.append(grb)
         grbs.close
 
-print np.shape(msgs)
 # Generate animated visualization
 file_name = "out/pic.gif"
 v.Animated(msgs, file_name)
