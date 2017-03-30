4bca8d062a501717fdc84c7d13d2e15736f29fdf
ddoshi26 <doshid@purdue.edu>
2017-03-30 13:49:37 -0700

    Modified DataComparator

--- wpear/DataComparator.py
+++ wpear/DataComparator.py
@@ -1,7 +1,8 @@
 import pygrib
 import numpy
+import DataVisualizer
 
-def DataComparator(f_msg, o_msg):
+def DataComparator(f_msg, o_msg, image_file):
 	#Potentially add column name too once we have more than one column in the cleaned file
 	#Comment out file opens if the file is sent
 	#grbs_f = pygrib.open(f_msg)
@@ -12,9 +13,11 @@ def DataComparator(f_msg, o_msg):
 	#print lons.min(), lons.max()
 	#Only doing difference for now
 	#print f_msg.values
+	dv = DataVisualizer.DataVisualizer()
 	arr = f_msg.values - o_msg.values
 	#print arr
 	gobj = f_msg
+	dv.HeatmapWithArgs(gobj, image_file, arr)
 	#gobj.values=arr#numpy.array(arr)
 	
 	#print gobj.values
@@ -22,9 +25,10 @@ def DataComparator(f_msg, o_msg):
 	# return grib message
 	return gobj
 
-def MultiSetAverage(grb_list):	# To change to index referencing: MultiSetAverage(grb_list, index)
+def MultiSetAverage(grb_list, image_file):	# To change to index referencing: MultiSetAverage(grb_list, index)
 	size = len(grb_list)
-	print grb_list[0].values
+	dv = DataVisualizer.DataVisualizer()
+	#print grb_list[0].values
 	if size <= 0:
 		return None
 	else:		
@@ -37,8 +41,9 @@ def MultiSetAverage(grb_list):	# To change to index referencing: MultiSetAverage
 			arr = arr + grb_obj.values		# Index referencing: arr = arr + grb_obj.message(index).values
 		
 		arr = arr/size
-		print arr
+		#print arr
 		
+		dv.HeatmapWithArgs(grb_obj[0], image_file, arr)	
 		return arr
 #MultiSetAverage([pygrib.open('hrrr.t15z.wrfsubhf01.grib2').select(name='2 metre temperature')[0], pygrib.open('hrrr.t15z.wrfsubhf01.grib2').select(name='2 metre temperature')[0]])
-#DataComparator(pygrib.open('in_hrrr.t00z.wrfsfcf00.grib2').select(name='Temperature')[0], pygrib.open('in_hrrr.t00z.wrfsfcf10.grib2').message(1))
+DataComparator(pygrib.open('in_hrrr.t00z.wrfsfcf00.grib2').select(name='Temperature')[0], pygrib.open('in_hrrr.t00z.wrfsfcf10.grib2').message(1), 'image_1.jpg')
--- wpear/PointAverage.py
+++ wpear/PointAverage.py
@@ -9,7 +9,7 @@ def DataSetAverage(data_set):
 	
 	return total/len(data_set) 	
 
-def PointAverage(p_lat, p_lon, f_list):
+def PointAverage(p_lat, p_lon, f_list, output_name):
 	lat_max = p_lat+0.03623189
 	lat_min = p_lat-0.03623189
 	lon_max = p_lon+0.04753755
@@ -23,8 +23,9 @@ def PointAverage(p_lat, p_lon, f_list):
 		#print avg_temp
 		avg_temp_list.append(avg_temp)
 		#print avg_temp_list
-	#plt.plot(avg_temp_list)
-	#plt.show()
+	plt.plot(avg_temp_list)
+	plt.show()
+	plt.savefig(output_name)
 	return avg_temp_list
 PointAverage(40.427289, -86.914522, [pygrib.open('hrrr.t15z.wrfsubhf01.grib2').select(name='2 metre temperature')[0], pygrib.open('hrrr.t00z.wrfsfcf01.grib2').select(name='2 metre temperature')[0], pygrib.open('hrrr.t15z.wrfsubhf01.grib2').select(name='2 metre temperature')[0]])
 			
