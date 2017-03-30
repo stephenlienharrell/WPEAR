388c2f45a899bd1fbf1225ddb95c8ff2caf9d863
ddoshi26 <doshid@purdue.edu>
2017-03-30 13:25:21 -0700

    Pushing DataComparator

--- wpear/DataComparator.py
+++ wpear/DataComparator.py
@@ -4,27 +4,27 @@ import numpy
 def DataComparator(f_msg, o_msg):
 	#Potentially add column name too once we have more than one column in the cleaned file
 	#Comment out file opens if the file is sent
-	#grbs_f = pygrib.open(forecast_file)
+	#grbs_f = pygrib.open(f_msg)
 	#grbs_o = pygrib.open(observed_file)
-	
-	#grb = grbs_f.message(12)
+	#for grb in f_msg:
+	#	print grb
 	#lats,lons = grb.latlons()
-	#print lats.min(), lats.max()
 	#print lons.min(), lons.max()
 	#Only doing difference for now
+	#print f_msg.values
 	arr = f_msg.values - o_msg.values
 	#print arr
 	gobj = f_msg
-	gobj.values=numpy.array(arr)
+	#gobj.values=arr#numpy.array(arr)
 	
-	print gobj.values
+	#print gobj.values
 
 	# return grib message
 	return gobj
 
 def MultiSetAverage(grb_list):	# To change to index referencing: MultiSetAverage(grb_list, index)
 	size = len(grb_list)
-
+	print grb_list[0].values
 	if size <= 0:
 		return None
 	else:		
@@ -37,8 +37,8 @@ def MultiSetAverage(grb_list):	# To change to index referencing: MultiSetAverage
 			arr = arr + grb_obj.values		# Index referencing: arr = arr + grb_obj.message(index).values
 		
 		arr = arr/size
-		#print arr
+		print arr
 		
 		return arr
-#MultiSetAverage([pygrib.open('hrrr.t15z.wrfsubhf01.grib2'), pygrib.open('hrrr.t15z.wrfsubhf01.grib2')], '2 metre temperature')
-#DataComparator(pygrib.open('hrrr.t15z.wrfsubhf01.grib2'), pygrib.open('hrrr.t15z.wrfsubhf01.grib2'))
+#MultiSetAverage([pygrib.open('hrrr.t15z.wrfsubhf01.grib2').select(name='2 metre temperature')[0], pygrib.open('hrrr.t15z.wrfsubhf01.grib2').select(name='2 metre temperature')[0]])
+#DataComparator(pygrib.open('in_hrrr.t00z.wrfsfcf00.grib2').select(name='Temperature')[0], pygrib.open('in_hrrr.t00z.wrfsfcf10.grib2').message(1))
