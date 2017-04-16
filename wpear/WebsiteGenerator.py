a9ac80a29a896338d885c5261a6bba7dcc973793
Dhairya Doshi <doshid@purdue.edu>
2017-04-16 00:51:50 -0400

    Now able to display images with titles in a line

--- wpear/WebsiteGenerator.py
+++ wpear/WebsiteGenerator.py
@@ -5,25 +5,30 @@ def showWebsite(item_list):
     html_file = open('main.html', 'w+')
     file_head = """<html><head>
     <title>WPEAR</title>
-    <body style="background-color: #EAEDED;"><center><p><h1><br>WPEAR</h1></p>"""
+    <body style="background-color: #EAEDED;"><center><p><h1><br>WPEAR</h1></center>"""
     html_file.write(file_head)
+    image_size = """'padding:20px;width:375px;height:500px;'"""
 
     for item in item_list:
         if 'SECTION:' in item:
             if item != item_list[0]:
-                html_file.write("""</div>""")
-            html_file.write("""<br><div id = """ + item.split(':')[1] +""""><h2><p>""" +  item.split(':')[1] + """</p></h2>""")
+                html_file.write("""<p style="clear: both;"><hr>""")
+            html_file.write("""<br><center><h2><p>""" +  item.split(':')[1] + """</p></h2></center>""")
         elif len(item) > 1:
-            html_file.write("""<h3>""" + item[1] + """</h3><img src='""" + item[0] + """' alt='""" + item[1] + """'><br>""")
-            html_file.write("""<br>""")
-    # <h2>""" + observed_name + """ Visualization</h2>
-    # <img src='""" + observed_image + """' alt="Observed file" >
-    # <h2>Compared Visualization</h2>
-    # <img src='""" + compared_image + """' alt="Compared file" >
+            html_file.write("""<p style="float:left;font-size:16pt;text-align:center;width:23%;margin-right:2%;">&emsp;""")
+            html_file.write(item[1] + """<img src='""" + item[0] + """' alt='""" + item[1] +
+                """' style="""+ image_size + """'></p>""")
 
-    file_end = """</center></body></html>"""
+    file_end = """</body></html>"""
     html_file.write(file_end)
     html_file.close()
 
     webbrowser.open_new('main.html')
-# showWebsite(['SECTION:Forecast', ['img.jpg', 'file_1'], ['img.jpg', 'file_2'], 'SECTION:Observed'])
+
+def parseDirectory(file_name):
+    directory = "web/"
+
+
+showWebsite(['SECTION:Forecast', ['giphy.gif', 'file_1'], ['giphy.gif', 'file_2'], ['giphy.gif', 'file_3'], ['giphy.gif', 'file_4'],
+['giphy.gif', 'file_1'], ['giphy.gif', 'file_2'], ['giphy.gif', 'file_3'], ['giphy.gif', 'file_4'],
+             'SECTION:Observed'])
