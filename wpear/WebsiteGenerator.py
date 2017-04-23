d8f219c128feb64c0f39db72a378d11668667333
Dhairya Doshi <doshid@purdue.edu>
2017-04-23 15:40:18 -0400

    Pushing static day website Generator

--- wpear/WebsiteGenerator.py
+++ wpear/WebsiteGenerator.py
@@ -1,34 +1,53 @@
 #from selenium import webdriver
 import webbrowser
+import os.path
 
 def showWebsite(item_list):
-    html_file = open('main.html', 'w+')
+    dir = parseDirectory('web/', '')
+    dir = os.path.normpath(dir + 'day.html')
+    file_fullpath = os.path.realpath(dir)
+    html_file = open(file_fullpath, 'w+')
+
     file_head = """<html><head>
-    <title>WPEAR</title>
+    <font size="3"><title>WPEAR</title>
     <body style="background-color: #EAEDED;"><center><p><h1><br>WPEAR</h1></center>"""
     html_file.write(file_head)
-    image_size = """'padding:20px;width:375px;height:500px;'"""
+    image_size = """'padding-left:60px; padding-right:60px;padding-top:20px;padding-bottom:20px;width:100%;height:80%;'"""
 
     for item in item_list:
         if 'SECTION:' in item:
             if item != item_list[0]:
                 html_file.write("""<p style="clear: both;"><hr>""")
             html_file.write("""<br><center><h2><p>""" +  item.split(':')[1] + """</p></h2></center>""")
+            continue
         elif len(item) > 1:
-            html_file.write("""<p style="float:left;font-size:16pt;text-align:center;width:23%;margin-right:2%;">&emsp;""")
+            html_file.write("""<p style="float:left;font-size:18pt;text-align:center;min-width:550px;margin-right:3%;">&nbsp;&nbsp;&nbsp;&nbsp;""")
             html_file.write(item[1] + """<img src='""" + item[0] + """' alt='""" + item[1] +
                 """' style="""+ image_size + """'></p>""")
+        #if len(item) > 2:
+        #    html_file.write(item[2])
 
     file_end = """</body></html>"""
     html_file.write(file_end)
     html_file.close()
 
-    webbrowser.open_new('main.html')
+    webbrowser.open_new(file_fullpath)
 
-def parseDirectory(file_name):
-    directory = "web/"
+def parseDirectory(webdir, file_name):
+    file_name = "hrrr_fcast.20170413_t00z.2MT_DPT.IND90k.wrfsfcf01.grb2"
+    directory = webdir
+    if directory.endswith('/') == False:
+        directory += '/'
 
+    seg = file_name.partition('.')
+    date_part = seg[len(seg)-1]
+    file_part = seg[0]
 
-showWebsite(['SECTION:Forecast', ['giphy.gif', 'file_1'], ['giphy.gif', 'file_2'], ['giphy.gif', 'file_3'], ['giphy.gif', 'file_4'],
-['giphy.gif', 'file_1'], ['giphy.gif', 'file_2'], ['giphy.gif', 'file_3'], ['giphy.gif', 'file_4'],
-             'SECTION:Observed'])
+    year = date_part[:4]
+    month = date_part[4:6]
+    day = date_part[6:8]
+
+    directory+= year + '/' + month + '/' + day + '/'
+    return directory
+
+#showWebsite(['SECTION:Forecast', ['giphy.gif', 'file_1'], ['giphy.gif', 'file_2'], ['giphy.gif', 'file_3'], ['giphy.gif', 'file_4'],['giphy.gif', 'file_1'], ['giphy.gif', 'file_2'], ['giphy.gif', 'file_3'], ['giphy.gif', 'file_4'],'SECTION:Observed'])
