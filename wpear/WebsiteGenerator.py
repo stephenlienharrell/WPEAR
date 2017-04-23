3a63cf3333da4e2d2e0165fb24d9e81bd14854d8
lvde21 <lvde21@live.com>
2017-04-23 16:43:47 -0400

    implemented sidebar calendar

--- wpear/WebsiteGenerator.py
+++ wpear/WebsiteGenerator.py
@@ -1,73 +1,200 @@
 import webbrowser, os
+from os import walk
 
 
 class WebsiteGenerator:
 
+  
+  def __init__(self, webdir, landing_file='index.html', sidebar_file='sidebar.html'):
+    
+    self.landing_file = webdir + '/' + landing_file
+    self.sidebar_file = webdir + '/' + sidebar_file
+    self.landing_page = open(self.landing_file, 'w+')
+    self.sidebar_page = open(self.sidebar_file, 'w+')
+    self.webdir = webdir
+    
 
-  landing_page = None
-  landing_sidebar = None
-  landing_main = None
-  landing_page_url = None
-  landing_sidebar_url = 'website/wpear_landing_sidebar.html'
-  landing_main_url = 'website/wpear_landing_main.html'
-
-
-  def __init__(self, file_name='website/index.html'):
-    # create website directory
-    cwd = os.getcwd()
-    website_dir = cwd+'/website'
-    if not os.path.exists(website_dir):
-      os.makedirs(website_dir)
-
-    self.landing_page_url = file_name
-    self.landing_page = open(file_name, 'w+')
-    self.landing_sidebar = open(self.landing_sidebar_url, 'w+')
-    self.landing_main = open(self.landing_main_url, 'w+')
-    self.addSidebar()
+  def showWebsite(self):
+    webbrowser.open_new(self.landing_file)
 
 
-  def showWebsite(self, item_list):
-    file_head = """<html><head>
-    <title>WPEAR</title>
-    <body style="background-color: #EAEDED;"><center><p><h1><br>WPEAR</h1></p>"""
-    self.landing_main.write(file_head)
-
-    for item in item_list:
-      if 'SECTION:' in item:
-        if item != item_list[0]:
-            self.landing_main.write("""</div>""")
-        self.landing_main.write("""<br><div id = """ + item.split(':')[1] +""""><h2><p>""" +  item.split(':')[1] + """</p></h2>""")
-      elif len(item) > 1:
-        self.landing_main.write("""<h3>""" + item[1] + """</h3><img src='""" + item[0] + """' alt='""" + item[1] + """'><br>""")
-        self.landing_main.write("""<br>""")
-    # <h2>""" + observed_name + """ Visualization</h2>
-    # <img src='""" + observed_image + """' alt="Observed file" >
-    # <h2>Compared Visualization</h2>
-    # <img src='""" + compared_image + """' alt="Compared file" >
-
-    file_end = """</center></body></html>"""
-    self.landing_main.write(file_end)
-    self.landing_main.close()
-
-    webbrowser.open_new(self.landing_page_url)
-
-
-  def addSidebar(self):
+  def addSidebarToLandingPage(self):
     sidebar = """
                 <HTML>
                 <HEAD>
                   <TITLE>Weather Prediction Evaluation and Reporting (WPEAR)</TITLE>
                 </HEAD>
                 <FRAMESET cols=20%,*>
-                <FRAME src="wpear_landing_sidebar.html" noresize frameborder="0" frameborder="0" scrolling="auto" />
-                <FRAME src="wpear_landing_main.html" name="page" noresize frameborder="0" scrolling="auto" />
+                <FRAME src="{}" noresize frameborder="0" frameborder="0" scrolling="auto" />
+                <FRAME src="" name="page" noresize frameborder="0" scrolling="auto" />
                 </frameset>
                 <NOFRAMES>
-              """
+              """.format(self.sidebar_file.split('/')[-1])
     self.landing_page.write(sidebar)
+    self.sidebarHandler()
+
+
+
+  def sidebarHandler(self):
+    self._writeSidebarHead()
+    self._writeSidebarBodyHeader()
+
+    years = []
+    for (dirpath, dirnames, filenames) in walk(self.webdir):
+      years.extend(dirnames)
+      break
+    years.sort()
+    for year in years:
+      yeardir = self.webdir + '/' + year
+      months = []
+      self._writeYearStart(year)
+      
+      for (ydirpath, ydirnames, yfilenames) in walk(yeardir):
+        months.extend(ydirnames)
+        break
+      months.sort()
+      for month in months:
+        monthdir = yeardir + '/' + month
+        days = []
+        self._writeMonthStart(month)
+
+        for (mdirpath, mdirnames, mfilenames) in walk(monthdir):
+          days.extend(mdirnames)
+          break
+        days.sort()
+        for day in days:
+          self._writeDay(day, monthdir + '/' + day + '/day.html')  
+
+        self._writeMonthEnd()
+      self._writeYearEnd()
+
+    # self._writeYearStart('2017')
+    # self._writeMonthStart('Jan')
+    # self._writeDay('12', 'index.html')
+    # self._writeDay('13', 'http://www.crh.noaa.gov/product.php?site=ind&product=AFD&issuedby=IND&glossary=0')
+    # self._writeMonthEnd()
+    # self._writeMonthStart('Dec')
+    # self._writeDay('12', 'index.html')
+    # self._writeDay('13', 'index2.html')
+    # self._writeMonthEnd()
+    # self._writeYearEnd()
+
+    # self._writeYearStart('2016')
+    # self._writeMonthStart('Jan')
+    # self._writeDay('12', 'index.html')
+    # self._writeDay('13', 'index2.html')
+    # self._writeMonthEnd()
+    # self._writeMonthStart('Dec')
+    # self._writeDay('12', 'index.html')
+    # self._writeDay('13', 'index2.html')
+    # self._writeMonthEnd()
+    # self._writeYearEnd()
+
+    self._writeSidebarBodyFooter()
 
 
 
 
+  def _writeSidebarHead(self):
+    head = """
+          <!DOCTYPE html>
+          <html>
+          <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+            <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+            <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+            <style>
+              .custom-btn {
+                  width: 9px !important;
+            }
+            </style>
+          </head> """
+    self.sidebar_page.write(head)      
 
-# showWebsite(['SECTION:Forecast', ['img.jpg', 'file_1'], ['img.jpg', 'file_2'], 'SECTION:Observed'])
+   
+  def _writeSidebarBodyHeader(self, title='WPEAR'):
+    entry = """
+              <body>
+              <div data-role="page" id="pageone">
+                <div data-role="header">
+                  <h1>%s</h1>
+                </div>
+                <div data-role="main" class="ui-content">
+                  <h2>Calendar</h2>""" % (title)
+    self.sidebar_page.write(entry)    
+
+
+  def _writeSidebarBodyFooter(self):
+    exit = """
+              </div>
+              <div data-role="footer">
+                <h1>Created by: Stephen Harrell,  <br/> Lala Vaishno De,  <br/> Mengxue Luo,  <br/> Dhairya Doshi</h1>
+              </div>
+
+            </div> 
+
+          </body>
+          </html>"""
+    self.sidebar_page.write(exit)
+
+
+  def _writeYearStart(self, year):
+    text = """
+              <div data-role="collapsible"  >
+              <h4>%s</h4>
+              <ul data-role="listview">""" % (year)
+    self.sidebar_page.write(text)
+
+
+  def _writeYearEnd(self):
+    text = """
+              </ul>
+            </div>"""
+    self.sidebar_page.write(text)
+
+
+  def _writeMonthStart(self, month):
+    text = """
+              <div data-role="collapsible">
+                <h4>%s</h4>    
+                  <ul data-role="listview">
+                    <div data-role="controlgroup" data-type="horizontal"  >""" % (month)
+    self.sidebar_page.write(text)
+
+
+  def _writeMonthEnd(self):
+    text = """
+                </div>
+              </ul>
+            </div>"""
+    self.sidebar_page.write(text)
+
+
+  def _writeDay(self, day, url):
+    text = """
+              <a target="page" href="%s" data-role="button" class="custom-btn">%s</a></li>""" % (url, day)
+    self.sidebar_page.write(text)
+
+  
+  def getLocalDirs(self, path):
+    dirs = []
+    for(dirpath, dirnames, filenames) in walk(path):
+      dirs.extend(dirnames)
+      break
+    return dirs
+
+  def getLocalFiles(self, path):  
+    files = []
+    for(dirpath, dirnames, filenames) in walk(path):
+      files.extend(filenames)
+      break
+    return files
+
+
+
+
+################################### Test run  script ####################################
+# wg = WebsiteGenerator(webdir='web')
+# wg.addSidebarToLandingPage()
+# wg.showWebsite()
