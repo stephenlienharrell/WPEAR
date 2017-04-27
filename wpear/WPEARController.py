1f138717843ae21af09cc50e5cf00fb3b8b3f724
sharrell@purdue.edu <sharrell@purdue.edu>
2017-04-27 19:53:34 -0400

    Analyze more days

--- wpear/WPEARController.py
+++ wpear/WPEARController.py
@@ -13,27 +13,50 @@ def StartRun(options):
     now = datetime.datetime.utcnow()
     one_day_delta = datetime.timedelta(days=1)
 
+    # downloading
+
     hrrr_dates = [now - one_day_delta, now]
     for date in hrrr_dates:
-        print 'Starting HRRR Forecasts for ' + date.strftime('%Y%m%d')
+        print 'Starting HRRR Downloading Forecasts for ' + date.strftime('%Y%m%d')
         hrrr_fcast = HRRRSurfaceForecasts.HRRRSurfaceForecasts(date,
                 VARS, DOMAIN, options, testing=options.testing)
         hrrr_fcast.DownloadData()
-        # hrrr_fcast.CleanupDownloads()
-        hrrr_fcast.VisualizeData()
-        hrrr_fcast.VisualizeAnimatedForecast()
 
     rtma_dates = [now - one_day_delta, now]
     for date in rtma_dates:
-        print 'Starting RTMA Observations for ' + date.strftime('%Y%m%d')
+        print 'Starting Downloading RTMA Observations for ' + date.strftime('%Y%m%d')
         rtma_obs = RTMAObservations.RTMAObservations(date,
                 VARS, DOMAIN, options, testing=options.testing)
         rtma_obs.DownloadData()
- #       rtma_obs.CleanupDownloads()
-        rtma_obs.VisualizeData()
-        rtma_obs.VisualizeDifference(hrrr_fcast, 'DIF')
-        rtma_obs.VisualizeAnimatedDifference(hrrr_fcast, 'ADIF')
-        rtma_obs.VisualizeStandardDeviation(hrrr_fcast)
+
+    hrrr_dates = []
+    rtma_dates = []
+    for i in reversed(range(0, 8)):
+        hrrr_dates.append(now - (one_day_delta * i))
+        rtma_dates.append(now - (one_day_delta * i))
+
+    for date in hrrr_dates:
+        hrrr_fcast = HRRRSurfaceForecasts.HRRRSurfaceForecasts(date,
+                VARS, DOMAIN, options, testing=options.testing)
+        rtma_obs = RTMAObservations.RTMAObservations(date,
+                VARS, DOMAIN, options, testing=options.testing)
+
+
+        if os.path.exists(hrrr_fcast.local_directory):
+            print 'Starting HRRR Forecasts for ' + date.strftime('%Y%m%d')
+            hrrr_fcast.VisualizeData()
+            hrrr_fcast.VisualizeAnimatedForecast()
+
+        if os.path.exists(rtma_obs.local_directory):
+            print 'Starting RTMA Observations for ' + date.strftime('%Y%m%d')
+            rtma_obs.VisualizeData()
+            rtma_obs.VisualizeDifference(hrrr_fcast, 'DIF')
+            rtma_obs.VisualizeAnimatedDifference(hrrr_fcast, 'ADIF')
+            rtma_obs.VisualizeStandardDeviation(hrrr_fcast)
+            
+        if os.path.exists(rtma_obs.local_directory) and os.path.exists(hrrr_fcast.local_directory):
+            wg = WebsiteGenerator.WebsiteGenerator(webdir = options.web_dir)
+            wg.runWebManager(hrrr_fcast, rtma_obs)
         
     # not sure how hrrr_fcast/rtma_obs exist out-of-scope
     # but it works
